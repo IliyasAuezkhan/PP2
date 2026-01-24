1523e3c43a83ec6c491ef1f0fa91d84f6655f516
IliyasAuezkhan <auezhanilias681@gmail.com>
2026-01-24 21:36:15 +0600

    Uploaded practice 1 assignments from the w3school

---
+++ Practice 1/Comments.py
@@ -0,0 +1,26 @@
+# 1 example
+#this is a comment
+
+# 2 example
+print("Valid") #This is comment
+
+# 3 example
+#print("lol")  (this code wouldn't work)
+print("bro")
+
+# 4 example
+#This is a comment
+#written in
+#more than just one line
+print("Among us")
+
+# 5 example
+"""
+This is a comment
+written in
+more than just one line
+multiline comment
+"""
+print("Are you crazy?!")
+
+
